2a440c95a936cde280ffec4402633797dcd024dc
jithin <jithinraj492@gmail.com>
2021-08-13 20:24:39 +0530

    before addig images

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // final _imagePaths = [
+  //   'images/img01.jpg',
+  //   'images/img02.jpg',
+  //   'images/img03.jpg',
+  //   'images/img04.jpg',
+  // ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          body: SafeArea(
+            child: Center(
+              child: CarouselSlider(
+                  options: CarouselOptions(autoPlay: true),
+                  items: _imagePaths.map((imagePath) {
+                    return Builder(builder: (context) {
+                      return Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: EdgeInsets.symmetric(horizontal: 4),
+                          child: Image.asset(imagePath));
+                    });
+                  }).toList()),
+            ),
+          ),
+        ));
+  }
+}
